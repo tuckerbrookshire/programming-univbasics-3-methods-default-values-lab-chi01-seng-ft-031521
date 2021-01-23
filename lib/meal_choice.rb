073e21a0105c7a4d733e42d75c8cf77c84f32d69
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-01-23 03:19:49 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice (veg1, veg2, protein= "tofu")
-  "What a nutritious meal!"
-  "A plate of #{protein} with #{veg1} and #{veg2}"
+def meal_choice (veg1, veg2, proteinn = "tofu")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
 end
